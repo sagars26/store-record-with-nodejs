18312468cd32d2d63f5d9c1096f2dbdd04965488
sagars26 <sagar260303@gmail.com>
2023-07-25 19:39:56 +0530

    setting up server

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/recordsdb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
