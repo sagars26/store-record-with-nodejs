39c74608b9b6afe321693b0f650d6de3646898d2
sagars26 <sagar260303@gmail.com>
2023-07-25 19:57:04 +0530

    defining data structure

---
+++ models/item.js
@@ -0,0 +1,14 @@
+// models/item.js
+
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  itemNo: { type: String, required: true, unique: true },
+  itemName: { type: String, required: true },
+  stockQuantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
